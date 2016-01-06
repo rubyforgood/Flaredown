aa045235bcaa8664850da3077d4f74226fdc7eae
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-06 16:41:28 +0100

    install and configure devise

---
+++ backend/config/initializers/action_mailer.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.default_url_options = { host: ENV['BASE_URL'] }
+
+ActionMailer::Base.smtp_settings = {
+  port:           ENV['SMTP_PORT'],
+  address:        ENV['SMTP_ADDRESS'],
+  user_name:      ENV['SMTP_USER'],
+  password:       ENV['SMTP_PASS'],
+  domain:         ENV['SMTP_DOMAIN'],
+  authentication: :login
+}
