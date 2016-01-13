09a454decaf95c870bb02b74fc97f91e2ef5eedf
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-13 11:28:16 +0100

    remove unused test, avoid to inject session into routes

--- frontend/app/instance-initializers/inject-session.js
+++ frontend/app/instance-initializers/inject-session.js
@@ -2,7 +2,6 @@ export default {
   name: 'injectSession',
 
   initialize: function initialize(application) {
-    application.inject('route', 'session', 'service:session');
     application.inject('controller', 'session', 'service:session');
     application.inject('component', 'session', 'service:session');
   }
---
+++ frontend/app/routes/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  }
+});
