08dfaf82ce3934251ab8bfaf30949be9441f2f58
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-13 09:37:23 +0100

    fix login error messages

--- frontend/app/components/login-form.js
+++ frontend/app/components/login-form.js
@@ -1,13 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  store: Ember.inject.service('store'),
-
   actions: {
     authenticateWithDevise() {
       let { identification, password } = this.getProperties('identification', 'password');
       this.get('session').authenticate('authenticator:devise', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error);
+        this.set('errorMessage', reason.errors);
       });
     }
   }
