bbf8854f2f8c3278920f6e75d722d9ac2e791978
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-19 22:31:36 +0100

    use facebook sdk

--- frontend/app/torii-providers/facebook.js
+++ frontend/app/torii-providers/facebook.js
@@ -1,10 +1,40 @@
 import Ember from 'ember';
-import FacebookOauth2 from 'torii/providers/facebook-oauth2';
+import FacebookConnect from 'torii/providers/facebook-connect';
 
-export default FacebookOauth2.extend({
+var fbPromise;
+
+function fbLoad(settings){
+  if (fbPromise) { return fbPromise; }
+  var original = window.fbAsyncInit;
+  var locale = settings.locale;
+  delete settings.locale;
+  fbPromise = new Ember.RSVP.Promise(function(resolve, reject){
+    window.fbAsyncInit = function(){
+      FB.init(settings);
+      Ember.run(null, resolve);
+    };
+    $.getScript('//connect.facebook.net/' + locale + '/sdk.js');
+  }).then(function(){
+    window.fbAsyncInit = original;
+    if (window.fbAsyncInit) {
+      window.fbAsyncInit.hasRun = true;
+      window.fbAsyncInit();
+    }
+  });
+
+  return fbPromise;
+}
+
+export default FacebookConnect.extend({
   session: Ember.inject.service('session'),
 
-  name:    'facebook',
-  redirectUri: Ember.computed.alias('session.extraSession.baseUrl'),
-  apiKey: Ember.computed.alias('session.extraSession.facebookAppId'),
+  name:  'facebook',
+  appId: Ember.computed.alias('session.extraSession.facebookAppId'),
+
+  setExtraSession: Ember.observer('session.extraSession', function() {
+    fbLoad( this.settings() );
+  }),
+
 });
+
+
