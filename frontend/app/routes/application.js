5c80a88f2d5545ebd03f6fb44bb606acfe821e5a
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-20 16:18:33 +0100

    setup Pace and improve header/footer setup

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -2,19 +2,6 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-
-  renderTemplate: function() {
-    this.render();
-    this.render('application/header', {
-      into: 'application',
-      outlet: 'header',
-    });
-    this.render('application/footer', {
-      into: 'application',
-      outlet: 'footer',
-    });
-  },
-
   actions: {
     invalidateSession() {
       this.get('session').invalidate();
