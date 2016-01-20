795874a772eb2cb2c49489c730bbd5fc373d8e5c
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-20 22:27:40 +0100

    real fix ember-simple-auth/issues/345fix ember-simple-auth/issues/345fix ember-simple-auth/issues/345

--- frontend/app/authenticators/facebook.js
+++ frontend/app/authenticators/facebook.js
@@ -5,6 +5,11 @@ export default ToriiAuthenticator.extend({
   ajax: Ember.inject.service(),
   torii: Ember.inject.service(),
 
+  restore: function(data) {
+    var resolveData = data || {};
+    this._provider = resolveData.provider;
+    return new Ember.RSVP.Promise(function(resolve) { resolve(resolveData); });
+  },
 
   authenticate() {
     return new Ember.RSVP.Promise((resolve, reject) => {
@@ -15,8 +20,10 @@ export default ToriiAuthenticator.extend({
         }).then((response) => {
           resolve({
             // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-            access_token: response.access_token,
+            user_id: response.user_id,
             // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+            email: response.email,
+            token: response.token,
             provider: data.provider
           });
         }, reject);
