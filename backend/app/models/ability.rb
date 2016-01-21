cf486c93c64da021b0c9cc81cd0b654e52390470
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-21 11:50:21 +0100

    fix the ability

--- backend/app/models/ability.rb
+++ backend/app/models/ability.rb
@@ -5,6 +5,6 @@ class Ability
     user ||= User.new # guest user (not logged in)
 
     can :manage, User, id: user.id
-    can :show, Profile, user_id: user.id
+    can :manage, Profile, user_id: user.id
   end
 end
