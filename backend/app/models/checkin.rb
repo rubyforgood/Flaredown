77829315ba7e479cfdf604ef65e8ff1397e52323
Salvatore Ferrucci <salvatore@algonauti.com>
2016-01-30 15:44:54 +0100

    add Tag relationship

--- backend/app/models/checkin.rb
+++ backend/app/models/checkin.rb
@@ -7,6 +7,10 @@ class Checkin
   field :date,       type: Date
   field :user_id,    type: Integer
 
+  #
+  # Relations
+  #
+  has_and_belongs_to_many :tags, class_name: 'Checkin::Tag'
 
   #
   # Validations
---
+++ backend/app/models/checkin/tag.rb
@@ -0,0 +1,5 @@
+class Checkin::Tag
+  include Mongoid::Document
+
+
+end
