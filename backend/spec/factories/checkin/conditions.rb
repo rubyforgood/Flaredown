1491f28459fbff8d01fe1062db12b363c5981d78
Davide Papagni <davide@algonauti.com>
2016-02-02 10:46:46 +0100

    checkin conditions and symptoms can be nil

--- backend/spec/factories/checkin/conditions.rb
+++ backend/spec/factories/checkin/conditions.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :checkin_condition, class: Checkin::Condition do
     transient do
-      values (0..4).to_a
+      values (0..4).to_a + [nil]
     end
     condition_id { FactoryGirl.create(:condition).id }
     value { values.sample }
