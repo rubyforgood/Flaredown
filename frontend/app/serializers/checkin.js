a8e8e3665600b51cc2c98fef0c532b281e700970
Davide Papagni <davide@algonauti.com>
2016-02-05 02:06:21 +0100

    checkin custom serializer

--- frontend/app/serializers/checkin.js
+++ frontend/app/serializers/checkin.js
@@ -2,9 +2,23 @@ import DS from 'ember-data';
 import { ActiveModelSerializer } from 'active-model-adapter';
 
 export default ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
+
   attrs: {
     conditions: { embedded: 'always' },
     symptoms: { embedded: 'always' },
     treatments: { embedded: 'always' }
-  }
+  },
+
+  serialize(snapshot, options) {
+    var json = this._super(...arguments);
+    json.conditions_attributes = json.conditions
+    delete json.conditions;
+    json.symptoms_attributes = json.symptoms
+    delete json.symptoms;
+    json.treatments_attributes = json.treatments
+    delete json.treatments;
+
+    return json;
+  },
+
 });
