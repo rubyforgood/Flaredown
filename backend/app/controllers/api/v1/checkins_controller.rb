8f0bfa2a2db8efcfaadc493812acd903333d70f6
Davide Papagni <davide@algonauti.com>
2016-02-05 01:55:35 +0100

    checkin API update method

--- backend/app/controllers/api/v1/checkins_controller.rb
+++ backend/app/controllers/api/v1/checkins_controller.rb
@@ -6,13 +6,32 @@ class Api::V1::CheckinsController < Api::BaseController
   end
 
   def show
-    render json: Checkin.find(params[:id])
+    render json: Checkin.find(id)
   end
 
   def create
     date = params.require(:checkin).require(:date)
     checkin = CheckinCreator.new(current_user.id, Date.parse(date)).create!
     render json: checkin
+  end
+
+  def update
+    checkin = Checkin.find(id)
+    checkin.update_attributes!(update_params)
+    render json: checkin
+  end
+
+  private
+
+  def update_params
+    params.require(:checkin).permit(:note,
+      conditions_attributes: [:id, :value, :condition_id, :color_id],
+      symptoms_attributes: [:id, :value, :symptom_id],
+      treatments_attributes: [:id, :value, :treatment_id])
+  end
+
+  def id
+    params.require(:id)
   end
 
 end
