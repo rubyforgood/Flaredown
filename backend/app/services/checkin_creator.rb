c0194b68770ef1ffc5f07f8f1c42d0f6aaff3724
Davide Papagni <davide@algonauti.com>
2016-02-09 11:41:54 +0100

    no need to initialize values to nil

--- backend/app/services/checkin_creator.rb
+++ backend/app/services/checkin_creator.rb
@@ -13,11 +13,11 @@ class CheckinCreator
     treatment_attrs = []
     active_trackables.each do |trackable|
       if trackable.is_a? Condition
-        condition_attrs << {condition_id: trackable.id, value: nil}
+        condition_attrs << {condition_id: trackable.id}
       elsif trackable.is_a? Symptom
-        symptom_attrs << {symptom_id: trackable.id, value: nil}
+        symptom_attrs << {symptom_id: trackable.id}
       elsif trackable.is_a? Treatment
-        treatment_attrs << {treatment_id: trackable.id, value: nil}
+        treatment_attrs << {treatment_id: trackable.id}
       end
     end
     Checkin.create!(
