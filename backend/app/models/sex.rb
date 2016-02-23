72ebc204319f9fd6c8fa3641edbd87c4625c606a
Davide Papagni <davide@algonauti.com>
2016-02-23 19:54:34 +0100

    refactor Sex to avoid class variable

--- backend/app/models/sex.rb
+++ backend/app/models/sex.rb
@@ -8,21 +8,33 @@ class Sex
     @rank = rank
   end
 
+  ALL_IDS =  %w( male female other doesnt_say )
+
   class << self
+
+    def find(id)
+      Store.instance.all.find { |sex| sex.id.eql?(id) }
+    end
+
     def all_ids
-      %w( male female other doesnt_say )
+      ALL_IDS
     end
 
     def all
-      @@all ||= begin
-        result = []
-        all_ids.each_with_index { |id, i| result << new(id, i + 1) }
-        result
-      end
+      Store.instance.all
     end
 
-    def find(id)
-      all.find { |sex| sex.id.eql?(id) }
+  end
+
+  class Store
+    include Singleton
+    attr_reader :all
+
+    def initialize
+      @all = []
+      ALL_IDS.each_with_index { |id, i| @all << Sex.new(id, i + 1) }
+      @all
     end
   end
+
 end
