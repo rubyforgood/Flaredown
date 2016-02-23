910859b774c8e8a38353c61f0305389ba3dea022
Davide Papagni <davide@algonauti.com>
2016-02-23 19:54:51 +0100

    fix rubocop offenses

--- backend/spec/models/search/dose_spec.rb
+++ backend/spec/models/search/dose_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Search::Dose, type: :model do
   let!(:treatment) { create(:checkin_treatment, checkin: checkin, is_taken: true, value: '2 x 10 mg') }
   let!(:other_treatments) { create_list(:checkin_treatment, 2, checkin: checkin, is_taken: true, treatment_id: treatment.treatment_id) }
   let(:all_treatment_doses) { [treatment.value] + other_treatments.map(&:value) }
-  let(:params) { {resource: 'dose', query: {treatment_id: treatment.treatment_id, name: '2 x'}} }
+  let(:params) { { resource: 'dose', query: { treatment_id: treatment.treatment_id, name: '2 x' } } }
 
   subject { Search::Dose.new(params) }
 
