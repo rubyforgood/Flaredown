688a6eb12bcfe2cdd0fd46bcf741f50e7fe864a7
Davide Papagni <davide@algonauti.com>
2016-02-24 17:31:24 +0100

    onboarding: implement demographic info step

--- frontend/app/components/onboarding/demographic-step.js
+++ frontend/app/components/onboarding/demographic-step.js
@@ -3,10 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   model: Ember.computed.alias('parentView.model'),
+  profile: Ember.computed.alias('model.profile'),
 
   actions: {
     completeStep() {
-      this.get('onStepCompleted')();
+      this.get('profile').save().then( () => {
+        this.get('onStepCompleted')();
+      });
     },
 
     goBack() {
