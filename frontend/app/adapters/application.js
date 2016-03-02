244435a25e2c86131788549dace37e576e8df087
Salvatore Ferrucci <salvatore@algonauti.com>
2016-03-02 09:21:38 +0100

    model-adapter: use default values

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -3,25 +3,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:devise',
-
-  namespace: 'api',
-  coalesceFindRequests: true,
-
-  shouldReloadAll: function() {
-    return true;
-  },
-
-  shouldBackgroundReloadAll: function() {
-    return true;
-  },
-
-  shouldReloadRecord: function() {
-    return false;
-  },
-
-  shouldBackgroundReloadRecord: function() {
-    return false;
-  }
-
+  namespace: 'api'
 });
 
