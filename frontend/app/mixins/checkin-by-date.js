69506ebb82ee20875ff151f895a1fe7b0b326d8d
Davide Papagni <davide@algonauti.com>
2016-03-08 18:57:31 +0100

    after login routing logic - fix #27

--- frontend/app/mixins/checkin-by-date.js
+++ frontend/app/mixins/checkin-by-date.js
@@ -8,18 +8,31 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
-  routeToCheckin: function(date, step) {
-    this.get('store').query('checkin', {date: date}).then(results => {
-      var records = results.toArray();
-      if (Ember.isEmpty(records)) {
-        var newCheckin = this.get('store').createRecord('checkin', {date: date});
-        newCheckin.save().then(savedCheckin => {
-          this.router.transitionTo('checkin', savedCheckin.get('id'), step ? step : 'start');
-        });
-      } else {
-        let checkin = records[0];
-        this.router.transitionTo('checkin', checkin.get('id'), step ? step : 'summary');
-      }
+  checkinByDate(date) {
+    return new Ember.RSVP.Promise((resolve,reject) => {
+      this.get('store').query('checkin', {date: date}).then(results => {
+        var records = results.toArray();
+        if (Ember.isEmpty(records)) {
+          reject();
+        } else {
+          resolve(records[0]);
+        }
+      });
+    });
+  },
+
+  routeToCheckin(date, step) {
+    this.checkinByDate(date).then( checkin => {
+      this.router.transitionTo('checkin', checkin.get('id'), step ? step : 'summary');
+    }, () => {
+      this.routeToNewCheckin(date, step);
+    });
+  },
+
+  routeToNewCheckin(date, step) {
+    var newCheckin = this.get('store').createRecord('checkin', {date: date});
+    newCheckin.save().then(savedCheckin => {
+      this.router.transitionTo('checkin', savedCheckin.get('id'), step ? step : 'start');
     });
   }
 
--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,7 +1,30 @@
+/* global moment */
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import OnboardedRoute from 'flaredown/mixins/onboarded-route';
+import CheckinByDate from 'flaredown/mixins/checkin-by-date';
 
-export default Ember.Route.extend(OnboardedRoute, AuthenticatedRouteMixin, {
+export default Ember.Route.extend(CheckinByDate, OnboardedRoute, AuthenticatedRouteMixin, {
+
+  /*
+    If the user has already checked-in today, default route when they open the app should be the chart.
+    If the user has no check-in today, default route should be today's check-in.
+  */
+  beforeModel(transition) {
+    if (this.get('session.isAuthenticated')) {
+      var date = moment(new Date()).format('YYYY-MM-DD');
+      this.checkinByDate(date).then(
+        () => {
+          return this._super(...arguments);
+        },
+        () => {
+          transition.abort();
+          this.routeToNewCheckin(date);
+        }
+      );
+    } else {
+      return this._super(...arguments);
+    }
+  }
 
 });
