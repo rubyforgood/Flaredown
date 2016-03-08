f28e4bf0674a828ef7f08e311fe465a6f6e939bf
Davide Papagni <davide@algonauti.com>
2016-03-08 19:30:53 +0100

    collapse after-login logic into one beforeModel hook - fix #30

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,27 +1,43 @@
 /* global moment */
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import OnboardedRoute from 'flaredown/mixins/onboarded-route';
 import CheckinByDate from 'flaredown/mixins/checkin-by-date';
 
-export default Ember.Route.extend(CheckinByDate, OnboardedRoute, AuthenticatedRouteMixin, {
+export default Ember.Route.extend(CheckinByDate, AuthenticatedRouteMixin, {
 
   /*
-    If the user has already checked-in today, default route when they open the app should be the chart.
-    If the user has no check-in today, default route should be today's check-in.
+    REQUIREMENTS:
+    If the user hasn't completed the onboarding process
+      default route is the last completed onboarding step
+    Else
+      If the user has already checked-in today
+        default route when they open the app should be the chart
+      Else
+        default route should be today's check-in
   */
   beforeModel(transition) {
     if (this.get('session.isAuthenticated')) {
-      var date = moment(new Date()).format('YYYY-MM-DD');
-      this.checkinByDate(date).then(
-        () => {
-          return this._super(...arguments);
-        },
-        () => {
-          transition.abort();
-          this.routeToNewCheckin(date);
-        }
-      );
+      this.get('session.currentUser').then(currentUser => {
+        currentUser.get('profile').then(profile => {
+          profile.get('onboardingStep').then(step => {
+            if (profile.get('isOnboarded')) {
+              var date = moment(new Date()).format('YYYY-MM-DD');
+              this.checkinByDate(date).then(
+                () => {
+                  return this._super(...arguments);
+                },
+                () => {
+                  transition.abort();
+                  this.routeToNewCheckin(date);
+                }
+              );
+            } else {
+              transition.abort();
+              this.transitionTo("onboarding", step.get('key'));
+            }
+          });
+        });
+      });
     } else {
       return this._super(...arguments);
     }
