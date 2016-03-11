282201b48e226ea50365c89f245dcc8fa3746228
Salvatore Ferrucci <salvatore@algonauti.com>
2016-03-11 12:15:07 +0100

    enable coalesceFindRequests

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -3,6 +3,7 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:devise',
-  namespace: 'api'
+  namespace: 'api',
+  coalesceFindRequests: true
 });
 
