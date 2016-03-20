c62f0703790dd91883c5f6b1aa160259b56387b6
Salvatore Ferrucci <salvatore@algonauti.com>
2016-03-20 10:40:51 +0100

    check default route into Router

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,8 +1,50 @@
+/* global moment */
 import Ember from 'ember';
+import CheckinByDate from 'flaredown/mixins/checkin-by-date';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType
+const Router = Ember.Router.extend(CheckinByDate, {
+  location: config.locationType,
+
+
+  /*
+    REQUIREMENTS:
+    If the user hasn't completed the onboarding process
+      default route is the last completed onboarding step
+    Else
+      If the user has already checked-in today
+        default route when they open the app should be the chart
+      Else
+        default route should be today's check-in
+  */
+  beforeModel(transition) {
+    if (this.get('session.isAuthenticated')) {
+      this.get('session.currentUser').then(currentUser => {
+        currentUser.get('profile').then(profile => {
+          profile.get('onboardingStep').then(step => {
+            if (profile.get('isOnboarded')) {
+              var date = moment(new Date()).format('YYYY-MM-DD');
+              this.checkinByDate(date).then(
+                () => {
+                  return this._super(...arguments);
+                },
+                () => {
+                  transition.abort();
+                  this.routeToNewCheckin(date);
+                }
+              );
+            } else {
+              transition.abort();
+              this.transitionTo("onboarding", step.get('key'));
+            }
+          });
+        });
+      });
+    } else {
+      return this._super(...arguments);
+    }
+  }
+
 });
 
 Router.map(function() {
