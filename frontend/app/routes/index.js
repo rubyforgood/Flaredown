3570ec077f6e71522b9526b04c9d60f5c8ddd6ae
Salvatore Ferrucci <salvatore@algonauti.com>
2016-03-20 11:24:20 +0100

    Revert "check default route into Router"
    This reverts commit c62f0703790dd91883c5f6b1aa160259b56387b6.

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,7 +1,46 @@
 /* global moment */
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import CheckinByDate from 'flaredown/mixins/checkin-by-date';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(CheckinByDate, AuthenticatedRouteMixin, {
+
+  /*
+    REQUIREMENTS:
+    If the user hasn't completed the onboarding process
+      default route is the last completed onboarding step
+    Else
+      If the user has already checked-in today
+        default route when they open the app should be the chart
+      Else
+        default route should be today's check-in
+  */
+  beforeModel(transition) {
+    if (this.get('session.isAuthenticated')) {
+      this.get('session.currentUser').then(currentUser => {
+        currentUser.get('profile').then(profile => {
+          profile.get('onboardingStep').then(step => {
+            if (profile.get('isOnboarded')) {
+              var date = moment(new Date()).format('YYYY-MM-DD');
+              this.checkinByDate(date).then(
+                () => {
+                  return this._super(...arguments);
+                },
+                () => {
+                  transition.abort();
+                  this.routeToNewCheckin(date);
+                }
+              );
+            } else {
+              transition.abort();
+              this.transitionTo("onboarding", step.get('key'));
+            }
+          });
+        });
+      });
+    } else {
+      return this._super(...arguments);
+    }
+  }
 
 });
