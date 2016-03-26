1d481b101b864d18a05e8cbd7ac57b5e4f2cbf15
Salvatore Ferrucci <salvatore@algonauti.com>
2016-03-26 19:24:27 +0100

    enable airbreak only in production

--- backend/config/initializers/airbrake.rb
+++ backend/config/initializers/airbrake.rb
@@ -2,4 +2,4 @@ Airbrake.configure do |config|
   config.ignore_environments = %w(development test)
   config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
-end
+end if Rails.env.production?
