5c766eb413ec1dee1084f05390f15795a9dca88b
Salvatore Ferrucci <salvatore@algonauti.com>
2016-03-30 00:50:32 +0200

    increase min pumas threads

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -6,8 +6,8 @@
 #
 rackup DefaultRackup
 
-port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
+port ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RACK_ENV') { 'development' }
 
 # === Cluster mode ===
 
@@ -15,22 +15,28 @@ environment ENV['RACK_ENV'] || 'development'
 #
 # The default is "0".
 #
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV.fetch('WEB_CONCURRENCY') { 2 })
 
 # Configure "min" to be the minimum number of threads to use to answer
 # requests and "max" the maximum.
 #
 # The default is "0, 16".
 #
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads_count = Integer(ENV.fetch('MAX_THREADS') { 16 })
 threads threads_count, threads_count
 
 # Preload the application before starting the workers; this conflicts with
 # phased restart feature. (off by default)
 preload_app!
 
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
 end
