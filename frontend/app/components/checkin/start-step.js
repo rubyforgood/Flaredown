eacd0843071d66c8b4d79e3740107a412d90938f
Davide Papagni <davide@algonauti.com>
2016-04-14 15:11:27 +0200

    fix checkin start screen

--- frontend/app/components/checkin/start-step.js
+++ frontend/app/components/checkin/start-step.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('step', this.store.findRecord('step', 'checkin-start'));
+  },
+
   model: Ember.computed.alias('parentView.model'),
 
   actions: {
