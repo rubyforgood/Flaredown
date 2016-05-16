b24ccad3be2b70b945c06a8b74b14d9629b13841
Davide Papagni <davide@algonauti.com>
2016-05-16 19:07:04 +0200

    add createdAt to user's ember model #21

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   //Attributes
   email: DS.attr('string'),
+  createdAt: DS.attr('date'),
 
   //Associations
   profile: DS.belongsTo('profile')
