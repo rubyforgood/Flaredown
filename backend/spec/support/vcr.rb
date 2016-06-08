fac9a06438c17253049a24cab568480ae78f7b30
Davide Papagni <davide@algonauti.com>
2016-06-08 09:42:15 +0200

    add and configure vcr and webmock gems

---
+++ backend/spec/support/vcr.rb
@@ -0,0 +1,10 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.default_cassette_options = { allow_playback_repeats: true }
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = true
+  c.ignore_localhost = true
+end
