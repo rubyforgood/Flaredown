d82724c136eed2bcba850f4496feaedcef564bd9
Davide Papagni <davide@algonauti.com>
2016-06-09 09:39:09 +0200

    use webmock in specs

--- backend/spec/support/vcr.rb
+++ backend/spec/support/vcr.rb
@@ -3,6 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.default_cassette_options = { allow_playback_repeats: true }
+  # comment the following line when you want WebMock to suggest you to stub requests
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.allow_http_connections_when_no_cassette = true
