dc58b9a2d8a9d6903b7b165ed863ee34e88b42e4
Davide Papagni <davide@algonauti.com>
2016-06-09 13:24:15 +0200

    signup Ember route #73

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
+  this.route('signup');
 
   this.route('discourse-sign-in', { path: '/discourse/sign_in' });
 
