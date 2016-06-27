069155fb8aadea1992b7d2b549e76bc6ebf53cc8
Salvatore Ferrucci <salvatore@algonauti.com>
2016-06-27 11:13:40 +0200

    use module instead of nested class in order to fix CD

--- backend/lib/google/recaptcha_verifier.rb
+++ backend/lib/google/recaptcha_verifier.rb
@@ -1,31 +1,33 @@
-class Google::RecaptchaVerifier
+module Google
+  class RecaptchaVerifier
 
-  def initialize
-    @secret = ENV['RECAPTCHA_SECRET_KEY']
-    @url = 'https://www.google.com/recaptcha/api/siteverify'
-  end
-
-  def self.exec(response, remoteip=nil)
-    self.new.invoke_service(response, remoteip)
-  end
-
-  def invoke_service(response, remoteip)
-    params = {
-      secret: @secret,
-      response: response,
-      remoteip: remoteip
-    }
-    response = connection.post @url, params
-    if response.status.eql?(200)
-      response_json = JSON.parse(response.body)
-      return response_json['success']
-    else
-      fail StandardError.new("#{self.class}##{__method__} Got HTTP #{response.status}")
+    def initialize
+      @secret = ENV['RECAPTCHA_SECRET_KEY']
+      @url = 'https://www.google.com/recaptcha/api/siteverify'
     end
-  end
 
-  def connection
-    Faraday.new(headers: { 'Accept' => 'application/json' })
-  end
+    def self.exec(response, remoteip=nil)
+      self.new.invoke_service(response, remoteip)
+    end
 
+    def invoke_service(response, remoteip)
+      params = {
+        secret: @secret,
+        response: response,
+        remoteip: remoteip
+      }
+      response = connection.post @url, params
+      if response.status.eql?(200)
+        response_json = JSON.parse(response.body)
+        return response_json['success']
+      else
+        fail StandardError.new("#{self.class}##{__method__} Got HTTP #{response.status}")
+      end
+    end
+
+    def connection
+      Faraday.new(headers: { 'Accept' => 'application/json' })
+    end
+
+  end
 end
