4131a8ad6721796e19c1cf7dce53bbad4059cecf
Davide Papagni <davide@algonauti.com>
2016-08-31 12:13:04 +0200

    init userengage widget on login and destroy on logout

--- frontend/app/services/session.js
+++ frontend/app/services/session.js
@@ -26,6 +26,32 @@ export default SessionService.extend({
     this.set('extraSession', this.get('dataStore').find('session', 1));
   })),
 
+  isUserengageSet: false,
+  userengage: Ember.inject.service(),
+  setupUserengage: Ember.on('init', Ember.observer('currentUser', function() {
+    let currentUser = this.get('currentUser');
+    if (Ember.isPresent(currentUser)) {
+      currentUser.then( user => {
+        user.get('profile').then( profile => {
+          this.get('userengage').initialize({
+            state: 'simple',
+            email: user.get('email'),
+            sex: profile.get('sex.id'),
+            country_code: profile.get('country.id'),
+            birth_date: profile.get('birthDate'),
+            education_level: profile.get('educationLevel.id'),
+            onboarded: profile.get('isOnboarded')
+          });
+          this.set('isUserengageSet', true);
+        });
+      });
+    } else {
+      if (this.get('isUserengageSet')) {
+        this.get('userengage').destroy();
+      }
+    }
+  })),
+
   setFullStoryUser: Ember.on('init', Ember.observer('currentUser', function() {
     let currentUser = this.get('currentUser');
     if (Ember.isPresent(currentUser)) {
