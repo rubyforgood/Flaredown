e34e3c9394c218779d0f1311b76e2b2b9f726340
Davide Papagni <davide@algonauti.com>
2016-09-22 23:55:01 +0200

    back to ember 2.6.0 and all related stuff

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'flaredown',
     environment: environment,
-    rootURL: '/',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -55,6 +55,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
+    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
