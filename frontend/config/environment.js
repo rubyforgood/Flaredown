86b5a2580cba91193167e4426e03e285c46d4714
Salvatore Ferrucci <salvatore@algonauti.com>
2016-11-14 10:10:50 +0100

    Revert "fix deprecation"
    This reverts commit af854b772457569e318053d6b2b48f42764b36b3.

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -10,9 +10,6 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        Date: false,
       }
     },
 
