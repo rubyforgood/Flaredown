40502d013d8ee7ac3884c530a5ed15fa8db9e0c1
Davide Papagni <davide@algonauti.com>
2016-11-26 07:49:23 +0100

    reusable create feature for EPS and 1st usage in tags-edit

--- frontend/app/components/checkin/tags-edit.js
+++ frontend/app/components/checkin/tags-edit.js
@@ -4,36 +4,27 @@ import SearchableDropdown from 'flaredown/mixins/searchable-dropdown';
 export default Ember.Component.extend(SearchableDropdown, {
 
   selectPlaceholder: Ember.computed('isSelectFocused', function() {
-    if (this.get('isSelectFocused')) {
+    if (this.get('isSelectFocused')) {   //TODO: make this work with EPS
       return 'Start typing a tag';
     } else {
       return 'Add a tag';
     }
   }),
 
-  onSelected: function(tag) {
-    this.get('onSelected')(tag);
-  },
-
   actions: {
     toggleSelectFocused() {
       this.toggleProperty('isSelectFocused');
     },
-    selected(selectedTag) {
-      // if it's a new tag, create it first and then pass it to upper action
-      if (Ember.isNone(selectedTag.get('id'))) {
-        selectedTag.save().then(tag => {
-          this.onSelected(tag);
-        });
-      } else {
-        this.onSelected(selectedTag);
-      }
-    },
     searchTags(term) {
       return this.searchByTerm('tag', term);
     },
-    handleChange() {
-      Ember.Logger.debug('TODO');
+    createTag(name) {
+      this.createAndSave('tag', name).then(tag => {
+        this.get('onSelected')(tag);
+      });
+    },
+    handleChange(tag) {
+      this.get('onSelected')(tag);
     },
     clicked(tag) {
       this.get('onClicked')(tag);
--- frontend/app/mixins/searchable-dropdown.js
+++ frontend/app/mixins/searchable-dropdown.js
@@ -12,6 +12,26 @@ export default Ember.Mixin.create({
     return this.store.queryRecord('search', params).then((searchRecord) => {
       return searchRecord.get('searchables');
     });
+  },
+
+  createAndSave(resource, term) {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      let record = this.store.createRecord(resource, { name: term });
+      record.save().then(function(saved) {
+        resolve(saved);
+      }, function() {
+        reject(...arguments);
+      });
+    });
+  },
+
+  actions: {
+    shouldShowCreateOption(term, options) {
+      let foundOption = options.find(function(item) {
+        return Ember.isEqual(term.toLowerCase(), item.get('name').toLowerCase());
+      });
+      return !Ember.isPresent(foundOption);
+    }
   }
 
 });
