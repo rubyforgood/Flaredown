107c1a373e4d403506d961016c2f65f0a6bffa2d
Maksym Pugach <pugach.m@gmail.com>
2016-12-29 15:03:50 +0200

    [#104] timezones fix

--- backend/app/services/weather_retriever.rb
+++ backend/app/services/weather_retriever.rb
@@ -5,17 +5,25 @@ class WeatherRetriever
     return weather if weather.present?
 
     position = Geocoder.search(postal_code).first
+    tz = Time.zone
+    Time.zone = NearestTimeZone.to(position.latitude, position.longitude)
 
     forecast = ForecastIO.forecast(
       position.latitude,
       position.longitude,
-      time: date.to_time.to_i,
+      time: Time.zone.parse(date.to_s).to_i,
       params: { exclude: 'minutely,hourly,alerts,flags' }
     )
 
+    Time.zone = tz
+
+    if Rails.application.secrets.debug
+      Rails.logger.info("\nThe forecast for #{date} and #{postal_code} is:\n#{forecast.to_yaml}\n")
+    end
+
     Weather.create(
       underscore_keys(permitted(forecast.daily.data.first)).merge(
-        date: date,
+        date: Date.strptime(forecast.daily.data.first.time.to_s, '%s'),
         postal_code: postal_code
       )
     )
