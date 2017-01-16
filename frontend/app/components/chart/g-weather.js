7f8db2cba4c2ec34ab10922ddbc02d00bb14fbd5
Maksym Pugach <pugach.m@gmail.com>
2017-01-16 13:08:26 +0200

    [#136] conditional show decimal rulers

--- frontend/app/components/chart/g-weather.js
+++ frontend/app/components/chart/g-weather.js
@@ -4,6 +4,34 @@ import Graphable from 'flaredown/components/chart/graphable';
 
 export default Ember.Component.extend(Colorable, Graphable, {
   colorId: '14',
+  rangeDevider: 4,
+
+  dataValuesYMin: Ember.computed.min('dataValuesY'),
+  dataValuesYMax: Ember.computed.max('dataValuesY'),
+
+  roundValuesYMin: Ember.computed('dataValuesYMin', function() {
+    return Math.floor(this.get('dataValuesYMin'));
+  }),
+
+  roundValuesYMax: Ember.computed('dataValuesYMax', function() {
+    return Math.ceil(this.get('dataValuesYMax'));
+  }),
+
+  isDecimalStep: Ember.computed('roundValuesYMin', 'roundValuesYMax', function() {
+    return this.get('roundValuesYMax') - this.get('roundValuesYMin') < this.get('rangeDevider');
+  }),
+
+  chosenYMin: Ember.computed('isDecimalStep', function() {
+    return this.get('isDecimalStep') ? this.get('dataValuesYMin') : this.get('roundValuesYMin');
+  }),
+
+  chosenYMax: Ember.computed('isDecimalStep', function() {
+    return this.get('isDecimalStep') ? this.get('dataValuesYMax') : this.get('roundValuesYMax');
+  }),
+
+  rulerStep: Ember.computed('roundValuesYMin', 'roundValuesYMax', function() {
+    return (this.get('chosenYMax') - this.get('chosenYMin')) / this.get('rangeDevider');
+  }),
 
   points: Ember.computed('data', function() {
     return(
@@ -28,17 +56,18 @@ export default Ember.Component.extend(Colorable, Graphable, {
   }),
 
   dataYValues: Ember.computed('data', function() {
-    const min = this.get('roundValuesYMin');
-    const max = this.get('roundValuesYMax');
-    const step = (max - min) / 4;
-
     let result = [];
 
-    for (let i = min; i < max; i += step) {
-      result.pushObject(Math.floor(i));
+    const finalizeNumber =this.get('isDecimalStep') ?
+      number => Math.round(number * 100) / 100
+    :
+      number => Math.floor(number);
+
+    for (let i = this.get('chosenYMin'); i < this.get('chosenYMax'); i += this.get('rulerStep')) {
+      result.pushObject(finalizeNumber(i));
     }
 
-    result.pushObject(max);
+    result.pushObject(this.get('chosenYMax'));
 
     return result.uniq();
   }),
@@ -56,26 +85,16 @@ export default Ember.Component.extend(Colorable, Graphable, {
     );
   }),
 
-  dataValuesYMax: Ember.computed.max('dataValuesY'),
-  dataValuesYMin: Ember.computed.min('dataValuesY'),
-
-  roundValuesYMax: Ember.computed('dataValuesYMax', function() {
-    return Math.ceil(this.get('dataValuesYMax'));
-  }),
-
-  roundValuesYMin: Ember.computed('dataValuesYMin', function() {
-    return Math.floor(this.get('dataValuesYMin'));
-  }),
-
   yScale: Ember.computed('data', function() {
-    let offset = (this.get('roundValuesYMax') - this.get('roundValuesYMin') + 1) / 4;
-
     return(
       d3
         .scale
         .linear()
         .range([this.get('height'), 0])
-        .domain([this.get('roundValuesYMin') - offset, this.get('roundValuesYMax') + offset])
+        .domain([
+          this.get('chosenYMin') - this.get('rulerStep'),
+          this.get('chosenYMax') + this.get('rulerStep')
+        ])
     );
   }),
 
