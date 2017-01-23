a383eec4e6049cba864e2776c3449ed50fccb705
Maksym Pugach <pugach.m@gmail.com>
2017-01-23 12:04:11 +0200

    [#17] adjust Userengage

--- frontend/app/routes/chart.js
+++ frontend/app/routes/chart.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'flaredown/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {});
+const {
+  $,
+  Route,
+} = Ember;
+
+const CHART_PATH_CLASS = 'charts-path';
+const TARGET = 'body';
+
+export default Route.extend(AuthenticatedRouteMixin, {
+  activate() {
+    $(TARGET).addClass(CHART_PATH_CLASS);
+  },
+
+  deactivate() {
+    $(TARGET).removeClass(CHART_PATH_CLASS);
+  },
+});
