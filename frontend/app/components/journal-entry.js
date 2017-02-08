6c8262e4108572d03ebdbddb3ff96ffd1245fe68
Maksym Pugach <pugach.m@gmail.com>
2017-02-08 11:48:34 +0200

    [#155] progressbar

--- frontend/app/components/journal-entry.js
+++ frontend/app/components/journal-entry.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 const {
   get,
+  set,
   computed,
   Component,
+  run: { scheduleOnce },
+  String: { htmlSafe },
 } = Ember;
 
 export default Component.extend({
+  symptoms: [],
   classNames: ["flaredown-white-box"],
+  symptomAverage: 0,
 
   date: computed('checkin.date', function() {
     return moment(get(this, 'checkin.date')).format("MMM D");
@@ -17,7 +22,25 @@ export default Component.extend({
     return get(this, 'checkin.note') || "No note was taken.";
   }),
 
+  style: computed('symptomAverage', function() {
+    return htmlSafe(`width: ${get(this, 'symptomAverage') * 100 / 5}%`);
+  }),
+
   checkinId: computed('checkin.id', function() {
     return get(this, 'checkin.id');
   }),
+
+  symptomCeil: computed('symptomAverage', function() {
+    return Math.round(get(this, 'symptomAverage'));
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    scheduleOnce('afterRender', this, () => {
+      get(this, 'checkin.symptoms').then(symptoms =>
+        set(this, 'symptomAverage', symptoms.reduce((acc, s) => acc + get(s, 'value'), 0) / symptoms.length)
+      );
+    });
+  },
 });
