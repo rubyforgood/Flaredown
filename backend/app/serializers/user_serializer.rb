829d3f16f2d034663bc707e41085849e59fd2323
Maksym Pugach <pugach.m@gmail.com>
2017-02-28 18:33:30 +0200

    [#198] save API call by embedding profile

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -27,7 +27,7 @@
 
 class UserSerializer < ApplicationSerializer
   attributes :id, :email, :intercom_hash
-  has_one :profile
+  has_one :profile, embed_in_root: true
 
   def intercom_hash
     OpenSSL::HMAC.hexdigest("sha256", ENV["INTERCOM_SECRET"], object.email)
