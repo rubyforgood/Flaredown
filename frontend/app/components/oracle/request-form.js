e744bf585765346a99f88add1aa6199726fd8120
Maksym Pugach <pugach.m@gmail.com>
2017-03-15 14:48:05 +0200

    [#211] move the feature into ‘oracle’ namespace

--- frontend/app/components/oracle/request-form.js
+++ frontend/app/components/oracle/request-form.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 const {
   Component,
+  inject: {
+    service,
+  },
   computed: {
     alias,
   },
 } = Ember;
 
 export default Component.extend({
+  selectableData: service(),
+
   sexes: alias('selectableData.sexes'),
 
   actions: {
