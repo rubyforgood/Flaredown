2c67362517317acbe19f4b10c76bf42602150431
Maksym Pugach <pugach.m@gmail.com>
2017-03-15 12:37:03 +0200

    [#223] use only defined locales

--- frontend/app/instance-initializers/i18n.js
+++ frontend/app/instance-initializers/i18n.js
@@ -2,7 +2,10 @@ export default {
   name: 'i18n',
 
   initialize(application) {
-    application.lookup('service:i18n').set('locale', navigator.language || 'en-GB');
+    let i18n = application.lookup('service:i18n');
+    const lang = navigator.language || navigator.userLanguage;
+
+    i18n.set('locale', i18n.get('locales').includes(lang) ? lang : 'en');
 
     application.inject('service:steps', 'i18n', 'service:i18n');
   },
