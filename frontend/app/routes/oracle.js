89783ca517bc1a2b614a25e0117cf43dca57e204
Maksym Pugach <pugach.m@gmail.com>
2017-03-15 15:53:40 +0200

    [#211] do not update url with query params

--- frontend/app/routes/oracle.js
+++ frontend/app/routes/oracle.js
@@ -5,13 +5,6 @@ const {
 } = Ember;
 
 export default Route.extend({
-  queryParams: {
-    age: { replace: true },
-    sexId: { replace: true },
-    countryId: { replace: true },
-    symptomIds: { replace: true },
-  },
-
   model() {
     return this.store.createRecord('oracleRequest', {});
   }
