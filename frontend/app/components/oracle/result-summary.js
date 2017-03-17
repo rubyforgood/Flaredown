f7e028ec2e401cff58c01a40afc015a995b37c6b
Maksym Pugach <pugach.m@gmail.com>
2017-03-17 13:32:15 +0200

    [#211] user can add conditions

--- frontend/app/components/oracle/result-summary.js
+++ frontend/app/components/oracle/result-summary.js
@@ -1,8 +1,38 @@
 import Ember from 'ember';
+import SearchableDropdown from 'flaredown/mixins/searchable-dropdown';
 
 const {
+  get,
+  computed,
   Component,
 } = Ember;
 
-export default Component.extend({
+export default Component.extend(SearchableDropdown, {
+  randomTrackables: computed(function() {
+    return this.randomSearch('condition');
+  }),
+
+  performSearch(term, resolve, reject) {
+    this
+      .searchByTerm('condition', term)
+      .then(function() { resolve(...arguments); }, reject);
+  },
+
+  actions: {
+    addCondition(condition) {
+      const name = get(condition, 'name');
+      const model = get(this, 'model');
+      const responce = get(model, 'responce');
+
+      if (!responce.findBy('name', name)) {
+        responce.pushObject({
+          name,
+          correction: 'selfAdded',
+          confidence: 100,
+        });
+
+        model.save();
+      }
+    }
+  },
 });
