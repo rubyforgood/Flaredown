ca40b5151c76c97e9bedee1dc2136b8993f92842
Maksym Pugach <pugach.m@gmail.com>
2017-03-17 15:50:08 +0200

    [#211] delayed spinner

--- frontend/app/components/oracle/request-form.js
+++ frontend/app/components/oracle/request-form.js
@@ -2,26 +2,41 @@ import Ember from 'ember';
 
 const {
   get,
+  set,
   Component,
   inject: {
     service,
   },
+  getProperties,
   computed: {
     alias,
   },
 } = Ember;
 
 export default Component.extend({
+  minSpinner: 3000,
+  maxSpinner: 5000,
   selectableData: service(),
+  oracleSpinnerVisible: false,
 
   sexes: alias('selectableData.sexes'),
 
   actions: {
     askOracle(oracleRequest) {
+      set(this, 'oracleSpinnerVisible', true);
+
+      const begin = + new Date();
+
       oracleRequest
         .askOracle()
         .then(() => oracleRequest.save())
-        .then(() => get(this, 'transitionToResult')(oracleRequest));
+        .then(() => {
+          const { minSpinner, maxSpinner } = getProperties(this, 'minSpinner', 'maxSpinner');
+
+          let timeout = Math.random() * (maxSpinner - minSpinner) + minSpinner - new Date() + begin;
+
+          setTimeout(() => get(this, 'transitionToResult')(oracleRequest), timeout > 0 ? timeout : 0);
+        });
     },
   },
 });
