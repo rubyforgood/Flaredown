fd9a58e527f677af7b5c818343a3380fc4df5e25
Maksym Pugach <pugach.m@gmail.com>
2017-03-28 17:14:45 +0300

    [#174] fetch comments in async when the are needed

--- backend/app/controllers/api/v1/comments_controller.rb
+++ backend/app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::CommentsController < ApplicationController
   load_and_authorize_resource
 
+  def index
+    render json: @comments.where(:id.in => params[:ids]).order_by(created_at: :asc)
+  end
+
+  def show
+    render json: @comment
+  end
+
   def create
     @comment.encrypted_user_id = current_user.encrypted_id
 
