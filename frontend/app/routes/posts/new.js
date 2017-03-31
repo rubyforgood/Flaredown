d5ee17a36e8a0f9c769bccf3f566465a1b273a31
Maksym Pugach <pugach.m@gmail.com>
2017-03-31 17:09:09 +0300

    [#171] remove unneeded mixin inclussion

--- frontend/app/routes/posts/new.js
+++ frontend/app/routes/posts/new.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'flaredown/mixins/authenticated-route-mixin';
 
 const {
   get,
   Route,
 } = Ember;
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend({
   model() {
     return get(this, 'store')
       .createRecord(
--- frontend/app/routes/posts/show.js
+++ frontend/app/routes/posts/show.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'flaredown/mixins/authenticated-route-mixin';
 
 const {
   get,
@@ -7,7 +6,7 @@ const {
   Route,
 } = Ember;
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend({
   model(params) {
     return get(this, 'store').find('post', params.id);
   },
