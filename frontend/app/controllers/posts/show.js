a75e1cb18798124d22ca368b24542fe0562bab20
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-05-29 11:20:33 +0300

    [#274] - Universal navbar

--- frontend/app/controllers/posts/show.js
+++ frontend/app/controllers/posts/show.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import BackNavigateable from 'flaredown/mixins/back-navigateable';
+import SearchableDropdown from 'flaredown/mixins/searchable-dropdown';
+import NavbarSearchable from 'flaredown/mixins/navbar-searchable';
 
 const {
   get,
@@ -19,7 +21,7 @@ const {
   },
 } = Ember;
 
-export default Controller.extend(BackNavigateable, {
+export default Controller.extend(BackNavigateable, SearchableDropdown, NavbarSearchable, {
   ajax: service(),
   notifications: service(),
 
