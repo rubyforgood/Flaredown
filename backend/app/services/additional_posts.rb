3e3b4a6edc081dcafa7f9bae2743f34a68dd8cc6
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-07-19 14:15:13 +0300

    [#289] - Add oneoff rake task

--- backend/app/services/additional_posts.rb
+++ backend/app/services/additional_posts.rb
@@ -7,7 +7,7 @@ class AdditionalPosts
 
   def add_last_new_posts(number, followed_ids)
     Post.where(_type: 'Post')
-      .not_in(:_id => followed_ids) # rubocop:disable Style/HashSyntax
+      .not_in('_id': followed_ids)
       .order(created_at: :desc)
       .to_a
       .first(number)
