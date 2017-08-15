06ce70f5b84472d80f5d4581f9f7b6d61ba3af9a
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-08-15 12:28:38 +0300

    [#333] - Fix link to profile page

--- frontend/app/mixins/postable-getable.js
+++ frontend/app/mixins/postable-getable.js
@@ -17,8 +17,8 @@ export default Mixin.create({
   getPostables(page, crossedTheLine) {
     const store = get(this, 'store');
 
-    const makeRequest = get(this, 'fastboot.isFastBoot') || crossedTheLine || get(this, 'fastboot.hasPeeked');
-    const promise = makeRequest ? store.query('postable', { page }) : RSVP.resolve(store.peekAll('postable'));
+    const makeRequest = get(this, 'fastboot.isFastBoot') || crossedTheLine;
+    const promise = makeRequest ? store.query('postable', { page }) : RSVP.resolve(this.peekData(store, page));
 
     return promise.then((postables) => {
       const fakePostable = get(postables, 'firstObject');
@@ -27,4 +27,10 @@ export default Mixin.create({
       return posts.toArray().concat(comments.toArray());
     });
   },
+
+  peekData(store, page) {
+    const postables = store.peekAll('postable');
+
+    return postables.toArray().length > 0 ? postables : store.query('postable', { page });
+  }
 });
