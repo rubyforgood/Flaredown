0cb9d625a7476e51f685f0eb8f3226336e9dac54
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-09-04 17:18:41 +0300

    [#289] - Change email

--- backend/app/jobs/group_top_posts_job.rb
+++ backend/app/jobs/group_top_posts_job.rb
@@ -6,7 +6,8 @@ class GroupTopPostsJob
     user = Profile.find_by(notify_token: notify_token)&.user
 
     return unless user
-    top_posts_ids = DiscussionPosts.new(params_hash, user).refined_top_list.pluck(:_id).map(&:to_s)
+
+    top_posts_ids = DiscussionPosts.new(params_hash, user).refined_top_list.map(&:id).map(&:to_s)
     TopPostsMailer.notify(email: user.email, notify_token: notify_token, top_posts_ids: top_posts_ids).deliver_later
   end
 end
