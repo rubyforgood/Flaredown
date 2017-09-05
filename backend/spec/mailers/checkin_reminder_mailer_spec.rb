8c61e2f2996f3ffe12bb9a58129a4f76d05da14c
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-09-05 15:38:46 +0300

    [#100] - Add test

--- backend/spec/mailers/checkin_reminder_mailer_spec.rb
+++ backend/spec/mailers/checkin_reminder_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe CheckinReminderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'checkin reminder email' do
+    let(:user_email) { create(:user).email }
+    let(:mail) { CheckinReminderMailer.remind(email: user_email) }
+
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq(I18n.t('checkin_reminder_mailer.subject'))
+      expect(mail.to).to eq([user_email])
+      expect(mail.from).to eq(['from@some.email'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match(I18n.t('checkin_reminder_mailer.body'))
+    end
+  end
 end
