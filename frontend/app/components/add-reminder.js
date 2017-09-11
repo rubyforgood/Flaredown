86e94aeff6cb0ebed351803faa2713437bdc0d0e
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-09-11 13:07:51 +0300

    [#100] - Change dropdown for time

--- frontend/app/components/add-reminder.js
+++ frontend/app/components/add-reminder.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import { translationMacro as t } from "ember-i18n";
 
 const {
+  get,
   set,
   computed,
   computed: {
@@ -30,13 +31,23 @@ export default Component.extend({
     return typeof moment !== 'undefined' && moment.tz && moment.tz.names();
   }),
 
+  options: computed(function() {
+    return {
+      hour: get(this, 'profile.checkinReminderAt.hours'),
+      mins: get(this, 'profile.checkinReminderAt.minutes'),
+      format: 'hh:i A'
+    }
+  }),
+
   actions: {
     changeReminder(boolParam) {
       set(this, 'profile.checkinReminder', boolParam);
     },
 
     timeChanged(param) {
-      set(this, 'profile.checkinReminderAt', param);
+      let parsedParam = { hours: param.getHours(), minutes: param.getMinutes() };
+
+      set(this, 'profile.checkinReminderAt', parsedParam);
     }
   },
 });
