a3d9853cda83ff5874705b349d803b9b477228ec
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-09-14 18:22:53 +0300

    [#100] - Add preview for reminder email with attachments

--- backend/spec/mailers/checkin_reminder_mailer_spec.rb
+++ backend/spec/mailers/checkin_reminder_mailer_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe CheckinReminderMailer, type: :mailer do
   describe 'checkin reminder email' do
-    let(:user_email) { create(:user).email }
+    let(:user_email) { create(:user, email: 'example@ex.com').email }
     let(:mail) { CheckinReminderMailer.remind(email: user_email) }
 
     it 'renders the headers' do
@@ -14,6 +14,10 @@ RSpec.describe CheckinReminderMailer, type: :mailer do
     it 'renders the body' do
       body = I18n.t('checkin_reminder_mailer.body', base_url: Rails.application.secrets.base_url)
       expect(mail.body.encoded.strip.tr('\"', '')).to match(body)
+    end
+
+    it 'renders the attachment' do
+      expect(mail.attachments.count).to eq 1
     end
   end
 end
