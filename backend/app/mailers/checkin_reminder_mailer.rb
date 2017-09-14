3b91b65ad1cf5466e4ed644578870bf582912180
Kristine <hrystyna.kozlovska@litslink.com>
2017-09-14 12:39:35 +0300

    [#100] - Add image to reminder email (#353)

--- backend/app/mailers/checkin_reminder_mailer.rb
+++ backend/app/mailers/checkin_reminder_mailer.rb
@@ -5,7 +5,9 @@ class CheckinReminderMailer < ApplicationMailer
     @email = notification_hash[:email]
     notify_token = User.find_by(email: @email).notify_token
     return unless notify_token
+
     @click_here_link = Rails.application.secrets.base_url
+    attachments.inline['optional_email_img.png'] = File.read('public/images/optional_email_img.png')
 
     mail(to: @email, subject: "Time to check in to Flaredown")
   end
