2e63a3957607cc53b1fa4afb6625426498d42691
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-09-22 10:45:58 +0300

    [#307] - show baseUrl on Fb sharing

--- frontend/app/components/share-socials.js
+++ frontend/app/components/share-socials.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 const {
+  computed,
   Component,
 } = Ember;
 
 export default Component.extend({
   classNames: ['shareSocials'],
   divideTitles: false,
+
+  baseUrl: computed(function() {
+    let location = window.location;
+
+    return location.protocol + "//" + location.host;
+  }),
 });
