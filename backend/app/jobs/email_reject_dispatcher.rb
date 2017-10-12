a3f85f177f27b33d21d31bcb1dfdb95b2c71c691
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-10-12 14:33:56 +0300

    [#359] - refactoring job

--- backend/app/jobs/email_reject_dispatcher.rb
+++ backend/app/jobs/email_reject_dispatcher.rb
@@ -12,14 +12,21 @@ class EmailRejectDispatcher
 
         { bounce: emails }
       elsif message_raw
-        message = JSON.parse message_raw
-
-        emails = message.dig('mail', 'destination') || []
-        rejected_type = message['notificationType'].downcase
-
-        { rejected_type.to_sym => emails }
+        generate_recipients(message_raw)
       end
 
+    return unless recipients.present?
+
     EmailRejectJob.perform_async(recipients)
+  end
+
+  def generate_recipients(message_raw)
+    message = JSON.parse message_raw
+
+    emails = message.dig('mail', 'destination') || []
+    rejected_type = message['notificationType'].downcase
+
+    { rejected_type.to_sym => emails }
+
   end
 end
