90aab4d11c048f85cfd1700a73be7d4749784831
Kristine.K <hrystyna.kozlovska@litslink.com>
2017-12-11 15:35:57 +0200

    Dynamic pickadate field in the days nav

--- frontend/app/components/checkin/days-nav.js
+++ frontend/app/components/checkin/days-nav.js
@@ -13,14 +13,17 @@ export default Component.extend(CheckinByDate, {
   classNames: ['checkin-head-nav'],
   classNameBindings: ['big'],
 
-  checkinDate: moment(),
-
   pickadateOptions: {
     container: 'body > .ember-view',
     max: moment().toDate(),
   },
 
   currentDate: Ember.computed.alias('checkin.date'),
+
+  checkinDate: computed('currentDate', function() {
+    return moment(get(this, 'currentDate')).toDate();
+  }),
+
   monthAndDay: Ember.computed('currentDate', function() {
     return moment(this.get('currentDate')).format("MMMM Do");
   }),
@@ -31,34 +34,29 @@ export default Component.extend(CheckinByDate, {
   isntToday: Ember.computed.not('isToday'),
 
   checkinDateLabel: computed('checkinDate', function() {
-      const checkinDate = get(this, 'checkinDate') || get(this, 'currentDate');
-
-      return moment(checkinDate).format('MMM D');
+    return moment(get(this, 'checkinDate')).format('MMM D');
   }),
 
   actions: {
     goToYesterday() {
       const yesterday = moment(this.get('currentDate')).subtract(1, 'days');
-      set(this, 'checkinDate', yesterday);
 
       this.routeToCheckin(yesterday.format("YYYY-MM-DD"), this.get('step.key'));
     },
     goToTomorrow() {
       if (this.get('isntToday')) {
         const tomorrow = moment(this.get('currentDate')).add(1, 'days');
-        set(this, 'checkinDate', tomorrow);
 
         this.routeToCheckin(tomorrow.format("YYYY-MM-DD"), this.get('step.key'));
       }
     },
 
     openPicker() {
-      $('.calendar-opener .picker__input').first().pickadate('picker').open();
+      this.$('.calendar-opener .picker__input').first().pickadate('picker').open();
     },
 
     checkinDateChanged(date) {
       const checkinDate = moment(date);
-      set(this, 'checkinDate', checkinDate);
 
       this.routeToCheckin(checkinDate.format("YYYY-MM-DD"), get(this, 'step.key'));
     },
