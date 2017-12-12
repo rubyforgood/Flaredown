7a329e5f225af6891e8c3d1a2eb2c28bddd3467a
Kristine <hrystyna.kozlovska@litslink.com>
2017-12-12 10:59:37 +0200

    Checkin datepicker (#380)

--- frontend/app/components/checkin/days-nav.js
+++ frontend/app/components/checkin/days-nav.js
@@ -1,31 +1,63 @@
 import Ember from 'ember';
 import CheckinByDate from 'flaredown/mixins/checkin-by-date';
 
-export default Ember.Component.extend(CheckinByDate, {
+const {
+  get,
+  computed,
+  Component,
+} = Ember;
 
-  classNames: ['checkin-days-nav'],
+export default Component.extend(CheckinByDate, {
+  classNames: ['checkin-head-nav'],
   classNameBindings: ['big'],
 
+  pickadateOptions: {
+    container: 'body > .ember-view',
+    max: moment().toDate(),
+  },
+
   currentDate: Ember.computed.alias('checkin.date'),
+
+  checkinDate: computed('currentDate', function() {
+    return moment(get(this, 'currentDate')).toDate();
+  }),
+
   monthAndDay: Ember.computed('currentDate', function() {
     return moment(this.get('currentDate')).format("MMMM Do");
   }),
+
   isToday: Ember.computed('currentDate', function() {
     return moment(this.get('currentDate')).isSame(new Date(), 'day');
   }),
   isntToday: Ember.computed.not('isToday'),
 
+  checkinDateLabel: computed('checkinDate', function() {
+    return moment(get(this, 'checkinDate')).format('MMM D');
+  }),
+
   actions: {
     goToYesterday() {
-      var yesterday = moment(this.get('currentDate')).subtract(1, 'days');
-      this.routeToCheckin(yesterday.format("YYYY-MM-DD"), this.get('step.key'));
+      const yesterday = moment(this.get('currentDate')).subtract(1, 'days');
+
+      this.routeToCheckin(yesterday.format("YYYY-MM-DD"), this.get('step.stepName'));
     },
     goToTomorrow() {
       if (this.get('isntToday')) {
-        var tomorrow = moment(this.get('currentDate')).add(1, 'days');
-        this.routeToCheckin(tomorrow.format("YYYY-MM-DD"), this.get('step.key'));
+        const tomorrow = moment(this.get('currentDate')).add(1, 'days');
+
+        this.routeToCheckin(tomorrow.format("YYYY-MM-DD"), this.get('step.stepName'));
       }
-    }
+    },
+
+    openPicker() {
+      this.$('.calendar-opener .picker__input').first().pickadate('picker').open();
+    },
+
+    checkinDateChanged(date) {
+      const checkinDate = moment(date);
+
+      this.routeToCheckin(checkinDate.format("YYYY-MM-DD"), get(this, 'step.stepName'));
+    },
   }
 
 });
