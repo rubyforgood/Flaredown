ecc30cf61fe137d2bc9613e83f31a24ee33f9d39
CW <compiledwrong+github@gmail.com>
2021-08-30 16:19:49 -0700

    move cors config into application.rb, staging login issues

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -19,6 +19,14 @@ Bundler.require(*Rails.groups)
 
 module Flaredown
   class Application < Rails::Application
+    # https://medium.com/@Nicholson85/handling-cors-issues-in-your-rails-api-120dfbcb8a24
+    # fix CORS issues in staging?
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
