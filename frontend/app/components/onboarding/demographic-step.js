48dc9e8b495521f91db8e993c48284162c61e4b5
Yunior Batista <yuniorbatista1113@gmail.com>
2021-10-13 17:36:09 -0400

    fixed negative input value on demographics page (#503)
    * fixed negative input value on demographics page
    
    * removed accidental console.log
    
    * commented out code removed from template
    
    * removed files not needed on PR and fixed suggestion

--- frontend/app/components/onboarding/demographic-step.js
+++ frontend/app/components/onboarding/demographic-step.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
   model: Ember.computed.alias('parentView.model'),
   profile: Ember.computed.alias('model.profile'),
 
@@ -18,6 +17,17 @@ export default Ember.Component.extend({
   }),
 
   actions: {
+    validateNumber(event) {
+      const inputVar = document.getElementById("input-num");
+
+      if(event.target.value > 24 || event.target.value < 0) {
+        alert("Must be a number between 0 - 24");
+        inputVar.value = "";
+        
+        return;
+      }
+      inputVar.disabled = false;
+    },
     completeStep() {
       this.get('profile').save().then( () => {
         this.get('onStepCompleted')();
