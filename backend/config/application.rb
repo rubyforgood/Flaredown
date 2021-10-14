712e1cdaa3d0e5c19a82d34dad22851efa697601
Ben Klang <bklang@powerhrg.com>
2021-10-14 14:44:12 -0400

    Fix removing a reaction from a discussion post (#530)
    * Allow DELETE requests in CORS
    
    This fixes removing emoji from posts. It extends the whitelist of HTTP verbs. Future work should consider whether allowing all origins is a good idea
    
    * Provide reaction IDs when available
    
    A real reaction ID is only required/meaningful when the current user has reacted. Otherwise the value is meaningless. Use a random string to try to demonstrate that in cases where a reaction is present on the post, but the requested user has not reacted.
    
    * Apply standardrb feedback

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -24,7 +24,7 @@ module Flaredown
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins "*"
-        resource "*", headers: :any, methods: [:get, :post, :options, :put]
+        resource "*", headers: :any, methods: [:get, :post, :options, :put, :delete]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
