1de6e3bd8d83308d2c0057fb6aa3adbde8adc6aa
compwron <compwron@users.noreply.github.com>
2023-05-23 19:56:54 -0700

    Add methods to cors ignore - yes we should fix this

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -24,7 +24,7 @@ module Flaredown
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins "*"
-        resource "*", headers: :any, methods: [:get, :post, :options, :put, :delete]
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
