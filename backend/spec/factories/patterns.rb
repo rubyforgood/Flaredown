dd133052d275c607efcd8378761b2442960c3c21
Peter Cho <pcho231@gmail.com>
2023-07-29 15:32:09 -0400

    * Add coverage for ChartPattern

---
+++ backend/spec/factories/patterns.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :pattern do
+    encrypted_user_id { create(:user).encrypted_id }
+    start_at { Time.zone.now }
+    end_at { Time.zone.now + 2.days.from_now }
+    name { 'Chuck Norris' }
+  end
+end
