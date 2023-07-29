25328d6b388c1a45cb58877ec1c03951dd509475
Sean Dickinson <contact@seandickinson.dev>
2023-07-29 11:38:49 -0400

    feat: working rswag test

---
+++ backend/spec/requests/api/v1/symptoms_spec.rb
@@ -0,0 +1,77 @@
+require "rails_helper"
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/symptoms', type: :request do
+  let!(:user) { create(:user) }
+
+  before { sign_in user }
+
+  path '/api/symptoms' do
+
+    get('list symptoms') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create symptom') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/symptoms/{id}' do
+
+    get('show symptom') do
+      parameter name: 'id', in: :path, type: :integer, description: 'id'
+      produces 'application/json'
+
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                 symptom: {
+                   type: :object,
+                   properties: {
+                     id: { type: :integer, required: true },
+                     updated_at: { type: :string, required: true },
+                     created_at: { type: :string, required: true },
+                     type: { type: :string, required: true },
+                     color_id: { type: :integer, nullable: true },
+                     users_count: { type: :integer, required: true },
+                     name: { type: :string, required: true },
+                   },
+                 }
+               },
+                required: [:symptom]
+
+        let(:id) { create(:symptom).id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
