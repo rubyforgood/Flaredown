da06219d2ffb7319fb2b613dc3d3122651542455
Sean Dickinson <contact@seandickinson.dev>
2023-07-29 18:51:32 -0400

    fix: simplify users swagger spec

--- backend/spec/requests/api/v1/users_spec.rb
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -1,21 +1,18 @@
-require 'swagger_helper'
+require "swagger_helper"
 
-RSpec.describe 'api/v1/users', type: :request do
+RSpec.describe "api/v1/users", type: :request do
   before { sign_in create(:user) }
 
-
-  path '/api/users/{id}' do
-    parameter name: 'id', in: :path, type: :string, description: 'id'
+  path "/api/users/{id}" do
+    parameter name: "id", in: :path, type: :string, description: "id"
 
     let(:id) { User.pick(:id) }
 
-
-    get('show user') do
-      response(200, 'successful') do
-
+    get("show user") do
+      response(200, "successful") do
         after do |example|
           example.metadata[:response][:content] = {
-            'application/json' => {
+            "application/json" => {
               example: JSON.parse(response.body, symbolize_names: true)
             }
           }
@@ -24,41 +21,13 @@ RSpec.describe 'api/v1/users', type: :request do
       end
     end
 
-    patch('update user') do
-      consumes 'application/json'
-      parameter name: :params, in: :body, schema: {
-        type: :object,
-        properties: {
-          user: {
-            type: :object,
-            properties: {
-              email: { type: :string },
-            }
-        }
-        },
-        required: [ 'user' ]
-      }
-      response(200, 'successful') do
-
-        let(:params) { {user: { email: 'updatedemail@example.com'} } }
+    patch("update user") do
+      response(200, "successful", use_as_request_example: true) do
+        let(:params) { {user: {email: "updatedemail@example.com"}} }
 
         after do |example|
           example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    put('update user') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
+            "application/json" => {
               example: JSON.parse(response.body, symbolize_names: true)
             }
           }
