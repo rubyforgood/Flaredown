e8db575a0cf2f620ba114e3ca30cc83438ff893f
Sean Dickinson <contact@seandickinson.dev>
2023-07-29 10:36:56 -0400

    feat: run rswag:ui:install

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 # rubocop:disable Metrics/BlockLength
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   root "application#root"
 
