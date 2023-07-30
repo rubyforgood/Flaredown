4be694ca4a1e88e27b859f238b5e549c3382a77d
Peter Cho <pcho231@gmail.com>
2023-07-30 10:55:36 -0400

    PR Feedback: JP

--- backend/spec/factories/patterns.rb
+++ backend/spec/factories/patterns.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :pattern do
     encrypted_user_id { create(:user).encrypted_id }
     start_at { Time.zone.now }
-    end_at { Time.zone.now + 2.days.from_now }
+    end_at { 2.days.from_now }
     name { "Chuck Norris" }
   end
 end
