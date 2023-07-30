3da577045ca6e0b9b93f0b3b457b13cfd301139c
Sean Dickinson <contact@seandickinson.dev>
2023-07-30 10:44:36 -0400

    fix: remove excess metadata on treatments

--- backend/spec/requests/api/v1/symptoms_spec.rb
+++ backend/spec/requests/api/v1/symptoms_spec.rb
@@ -1,4 +1,3 @@
-require "rails_helper"
 require "swagger_helper"
 
 RSpec.describe "api/v1/symptoms", type: :request do
@@ -8,23 +7,6 @@ RSpec.describe "api/v1/symptoms", type: :request do
       produces "application/json"
 
       response(200, "successful") do
-        schema type: :object,
-          properties: {
-            symptom: {
-              type: :object,
-              properties: {
-                id: {type: :integer, required: true},
-                updated_at: {type: :string, required: true},
-                created_at: {type: :string, required: true},
-                type: {type: :string, required: true},
-                color_id: {type: :integer, nullable: true},
-                users_count: {type: :integer, required: true},
-                name: {type: :string, required: true}
-              }
-            }
-          },
-          required: [:symptom]
-
         let(:id) { create(:symptom).id }
 
         after do |example|
@@ -34,8 +16,7 @@ RSpec.describe "api/v1/symptoms", type: :request do
             }
           }
         end
-        pending "Not yet implemented, when implemented uncomment the assertion below"
-        # run_test!
+        run_test!
       end
     end
   end
