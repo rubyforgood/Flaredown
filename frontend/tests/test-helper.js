540e77b8c7b1c46f22d5d8c7d2dd7a7eef88506d
Leo Correa <lcorr005@gmail.com>
2023-08-25 01:20:29 -0400

    Fix deprecation warning in tests
    DEPRECATION: `setResolver` should be imported from `@ember/test-helpers`, but was imported from `ember-qunit` [deprecation id: ember-qunit.deprecated-reexports.setResolver]

--- frontend/tests/test-helper.js
+++ frontend/tests/test-helper.js
@@ -1,10 +1,7 @@
 import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
 import Application from '../app';
 import config from '../config/environment';
-import { setApplication } from '@ember/test-helpers';
+import { setApplication, setResolver } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
