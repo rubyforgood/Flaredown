89223d341d7895d37a900fbdc6fdbc7df880927d
Leo Correa <lcorr005@gmail.com>
2023-09-04 12:11:22 -0400

    v2.18.2...v3.4.4

--- frontend/testem.js
+++ frontend/testem.js
@@ -14,5 +14,20 @@ module.exports = {
     "/api": {
       "target": "http://localhost:3000"
     }
+  },
+  browser_args: {
+    Chrome: {
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
+        '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
   }
 };
