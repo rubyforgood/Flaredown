d299a07a84c61e50b7840765eb84e184fd318147
Leo Correa <lcorr005@gmail.com>
2023-09-24 14:02:54 -0400

    Move from authorizers to authorize function
    authorizers were deprecated and removed in v2 of ember-simple-auth. This
    is a pretty old version of the addon but doing these small upgrades
    should help in the long run

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -3,9 +3,15 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'flaredown/config/environment';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:devise',
   host: ENV.apiHost,
   namespace: 'api',
-  coalesceFindRequests: true
+  coalesceFindRequests: true,
+  // defaults
+  // identificationAttributeName: 'email'
+  // tokenAttributeName: 'token'
+  authorize(xhr) {
+    let { email, token } = this.get('session.data.authenticated');
+    let authData = `Token token="${token}", email="${email}"`;
+    xhr.setRequestHeader('Authorization', authData);
+  }
 });
-
