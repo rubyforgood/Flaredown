245c62ad0f2b55d8f475ab64ae5b3cc6e5bd9ed9
Hussam Ghazzi <hussam-i-am@github.com>
2023-10-05 10:59:12 -0400

    dockerize

---
+++ native/webpack.config.js
@@ -0,0 +1,21 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  // If you want to add a new alias to the config.
+  config.entry = ['./node_modules/expo/AppEntry.js']
+
+  // Maybe you want to turn off compression in dev mode.
+  if (config.mode === 'development') {
+    config.devServer.compress = false;
+  }
+
+  // Or prevent minimizing the bundle when you build.
+  if (config.mode === 'production') {
+    config.optimization.minimize = false;
+  }
+
+  // Finally return the new config for the CLI to use.
+  return config;
+};
