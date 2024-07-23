d6cf7a333007a825e2e1843c57040403afe539fd
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-23 17:11:31 -0400

    development env config remove old options, clean

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,16 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    # Bullet.growl         = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -70,32 +60,13 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-
-  # NOTE: previously: -----
-  # # Debug mode disables concatenation and preprocessing of assets.
-  # # This option may cause significant delays in view rendering with a large
-  # # number of complex assets.
-  # config.assets.debug = true
-
-  # # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # # yet still be able to expire them through the digest params.
-  # config.assets.digest = true
-
-  # # Adds additional error checking when serving assets at runtime.
-  # # Checks for improperly declared sprockets dependencies.
-  # # Raises helpful error messages.
-  # config.assets.raise_runtime_errors = true
-
-  # # Raises error for missing translations
-  # # config.action_view.raise_on_missing_translations = true
-  # -------------
 
   # Delivery with letter opener
   config.action_mailer.delivery_method = :letter_opener
@@ -105,6 +76,8 @@ Rails.application.configure do
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
+    # Bullet.growl = true
     Bullet.rails_logger = true
+    Bullet.add_footer = true
   end
 end
